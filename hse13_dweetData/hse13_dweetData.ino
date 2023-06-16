572622f63f2c8e82dae9773eb137f8781d91bea1
esmoreido <moreido@mail.ru>
2023-06-16 12:13:19 +0300

    dweet added

---
+++ hse13_dweetData/hse13_dweetData.ino
@@ -0,0 +1,69 @@
+#include <ESP8266WiFi.h>                                     // Подключаем библиотеку ESP8266WiFi
+#include <TroykaThermometer.h>
+
+char ssid[]     = "WSHYDRO";               // Укажите здесь в кавычках название вашей точки доступа
+char password[] = "d7f4r5aes";            // Укажите здесь в кавычках пароль вашей точки доступа
+
+const char* host     = "dweet.io";                              // Адрес сервера на который будем отправлять данные
+
+TroykaThermometer thermometer(A7);                                      // Создаем объект датчика
+// адрес двита
+String thing = "termoEsmo";
+String content = "temperature";
+void setup() {
+  Serial.begin(9600);                                         // Инициализируем последовательный порт
+  delay(10);
+
+  // Подключаемся к точке доступа WiFi
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // считывание данных с датчика
+  thermometer.read();
+  float temp = thermometer.getTemperatureC();                              // Считываем данные температуры
+  Serial.print("Temperature = ");
+  Serial.println(temp);
+  WiFiClient client;
+  if (!client.connect("dweet.io", 80)) {
+    Serial.println("connection failed");
+    delay(5000);
+    return;
+  }
+  Serial.println("sending data to server");
+  if (client.connected()) {
+    // формируем GET-запрос для отправки
+  String url = "/dweet/for/";
+  url += thing;
+  url += "?";
+  url += content;
+  url += "=";
+  url += temp;
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+  
+     client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  } else {
+    Serial.println("Connection failed!");
+  }
+  delay(1000);
+  
+}
