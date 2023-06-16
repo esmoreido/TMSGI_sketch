349046c391266183e73d7730a81ec4205df8198c
esmoreido <moreido@mail.ru>
2023-06-16 13:13:50 +0300

    dweet docs added

--- hse13_dweetData/hse13_dweetData.ino
+++ hse13_dweetData/hse13_dweetData.ino
@@ -1,15 +1,15 @@
 #include <ESP8266WiFi.h>                                     // Подключаем библиотеку ESP8266WiFi
-#include <TroykaThermometer.h>
+#include <TroykaThermometer.h> // Подключаем библиотеку для аналогового термометра
 
-char ssid[]     = "WSHYDRO";               // Укажите здесь в кавычках название вашей точки доступа
-char password[] = "d7f4r5aes";            // Укажите здесь в кавычках пароль вашей точки доступа
+char ssid[]     = "";               // Укажите здесь в кавычках название вашей точки доступа
+char password[] = "";            // Укажите здесь в кавычках пароль вашей точки доступа
 
 const char* host     = "dweet.io";                              // Адрес сервера на который будем отправлять данные
 
-TroykaThermometer thermometer(A7);                                      // Создаем объект датчика
+TroykaThermometer thermometer(A7);                                      // Создаем объект датчика и указываем соответствующий порт
 // адрес двита
-String thing = "termoEsmo";
-String content = "temperature";
+String thing = "";          // В кавычках указываем любое название для стрима, например MyStreamTempSensor2023, тогда url для просмотра будет выглядеть как https://dweet.io/follow/MyStreamTempSensor2023
+String content = ""; // Название для переменной, которое будет отображаться в стриме
 void setup() {
   Serial.begin(9600);                                         // Инициализируем последовательный порт
   delay(10);
@@ -21,7 +21,7 @@ void setup() {
   Serial.println(ssid);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-
+  // ждем соединения с сетью
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -39,12 +39,15 @@ void loop() {
   float temp = thermometer.getTemperatureC();                              // Считываем данные температуры
   Serial.print("Temperature = ");
   Serial.println(temp);
+  
+  // создаем объект для передачи запроса
   WiFiClient client;
   if (!client.connect("dweet.io", 80)) {
     Serial.println("connection failed");
     delay(5000);
     return;
   }
+  
   Serial.println("sending data to server");
   if (client.connected()) {
     // формируем GET-запрос для отправки
@@ -57,7 +60,7 @@ void loop() {
 
   Serial.print("Requesting URL: ");
   Serial.println(url);
-  
+  // отправляем запрос 
      client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" + 
                "Connection: close\r\n\r\n");
